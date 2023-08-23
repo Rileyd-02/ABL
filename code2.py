ec1bce2cb91ea1baf18b5b75fa2aa9d7c1ff1fa4
TheekshithaVaratharajsarma <theekshithavs@gmail.com>
2023-08-23 12:00:37 +0530

    Theekshitha_member2

---
+++ code2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#memeber 2
+
+    # Initialize the cumulative_diff dictionary and other necessary variables
+    cumulative_diff = {0: -1}
+    max_length = 0
+    current_diff = 0
+
+    # Continue with the rest of the code...
+
