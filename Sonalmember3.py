992f35000c357f2b9cc5d264679cb14aba168cc5
Sonal-Punchihewa <COHNDDS231F-002@student.nibm.lk>
2023-08-23 12:04:54 +0530

    Sonal Member 3

---
+++ Sonalmember3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#member 03
+
+    for i in range(len(arr)):
+        # Calculate the current_diff based on the current element (0 or 1)
+        if arr[i] == 0:
+            current_diff -= 1
+        else:
+            current_diff += 1
+
+        # Continue with the rest of the code...
+
