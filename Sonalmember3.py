faa7f468def4cb1a45381424994be35e13014414
Sonal-Punchihewa <129730789+Sonal-Punchihewa@users.noreply.github.com>
2023-08-23 12:06:07 +0530

    Sonalmember3.py

--- Sonalmember3.py
+++ Sonalmember3.py
@@ -7,7 +7,7 @@
 #member 03
 
     for i in range(len(arr)):
-        # Calculate the current_diff based on the current element (0 or 1)
+        
         if arr[i] == 0:
             current_diff -= 1
         else:
