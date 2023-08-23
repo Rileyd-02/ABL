4266009b4a1ebb854a4f289b1c35eb780bea08a7
Rileyd-02 <129730881+Rileyd-02@users.noreply.github.com>
2023-08-23 11:53:57 +0530

    Riley member - 05

---
+++ Riley-5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+    return max_length
+
+arr1 = [0, 1, 0, 1, 1, 0, 0]
+result1 = longest_equal_subarray(arr1)
+print(result1)  
+
+arr2 = [1, 0, 1, 1, 1, 0]
+result2 = longest_equal_subarray(arr2)
+print(result2)  
+
+arr3 = [0, 1, 0, 1, 1, 0, 1, 0, 0, 1]
+result3 = longest_equal_subarray(arr3)
+print(result3)  
+
+
+# This code defines the "longest_equal_subarray" function and includes explanations of the steps involved. It then tests the function on three different arrays and prints the results.
