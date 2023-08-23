a9c22924b0e01009e06e20a4d91d46f4da851f07
Dinuk Ranaweera <129730913+Dinuk2705@users.noreply.github.com>
2023-08-23 11:33:08 +0530

    Dinuk Member 4 file

---
+++ Member4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+if current_diff in cumulative_diff:
+    subarray_length = i - cumulative_diff[current_diff]
+    max_length = max(max_length, subarray_length)
+else:
+    cumulative_diff[current_diff] = i
+
